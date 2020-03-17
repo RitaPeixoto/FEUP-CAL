d8bebd1139acc15244e75878c6737396af4f1863
RitaPeixoto <rita.peixoto2000@gmail.com>
2020-03-17 19:00:24 +0000

    Not passing the NP_DC tests

--- 3/cal_fp03_CLion/Tests/NearestPoints.cpp
+++ 3/cal_fp03_CLion/Tests/NearestPoints.cpp
@@ -39,11 +39,20 @@ static void sortByY(vector<Point> &v, int left, int right)
 }
 
 /**
- * Brute force algorithm O(N^2).
+ * Brute force algorithm O(N^2)-> it is O(n^2) because we have a for inside a for both iterating over a vector of size n
  */
 Result nearestPoints_BF(vector<Point> &vp) {
 	Result res;
-	// TODO
+	for(int i=0; i<vp.size();i++){
+	    for(int j=i+1;j<vp.size();j++){
+	        double distance = vp[i].distance(vp[j]);//distance between the two points
+	        if(distance<res.dmin){//verifies if the new distance is smaller
+	            res.p1 = vp[i];
+	            res.p2 = vp[j];
+                res.dmin = distance;//updates with the new minimum distance
+	        }
+	    }
+	}
 	return res;
 }
 
@@ -51,10 +60,8 @@ Result nearestPoints_BF(vector<Point> &vp) {
  * Improved brute force algorithm, that first sorts points by X axis.
  */
 Result nearestPoints_BF_SortByX(vector<Point> &vp) {
-	Result res;
 	sortByX(vp, 0, vp.size()-1);
-	// TODO
-	return res;
+	return nearestPoints_BF(vp);
 }
 
 
@@ -66,7 +73,19 @@ Result nearestPoints_BF_SortByX(vector<Point> &vp) {
  */
 static void npByY(vector<Point> &vp, int left, int right, Result &res)
 {
-	// TODO
+	for(int i = left; i<= right; i++){
+	    for(int j= left+1; j<=right; j++){
+	        double distance = vp[i].distance(vp[j]);
+	        if(distance>=res.dmin){
+	            break;
+	        }
+	        else if(distance<res.dmin){
+	            res.p1= vp[i];
+	            res.p2 = vp[j];
+	            res.dmin = vp[i].distance(vp[j]);
+	        }
+	    }
+	}
 }
 
 /**
@@ -75,32 +94,67 @@ static void npByY(vector<Point> &vp, int left, int right, Result &res)
  * using at most numThreads.
  */
 static Result np_DC(vector<Point> &vp, int left, int right, int numThreads) {
-	// Base case of two points
-	// TODO
+	Result res;
+    // Base case of two points
+	if(abs(left-right)==1){
+	    res.p1 = vp[left];
+	    res.p2 = vp[right];
+	    res.dmin = vp[left].distance(vp[right]);
+        return res;
+	}
 
 	// Base case of a single point: no solution, so distance is MAX_DOUBLE
-	// TODO
-
+	if(abs(left-right)==0){
+        res.p1 = vp[left];
+        res.p2 = vp[right];
+	    res.dmin = MAX_DOUBLE;
+	    return res;
+	}
 	// Divide in halves (left and right) and solve them recursively,
 	// possibly in parallel (in case numThreads > 1)
-	// TODO
+	//first we calcullate the middle index
+	int middle = left + abs(left-right)/2.0;
 
-	// Select the best solution from left and right
-	// TODO
+	//we're going to have two results  auxiliar results
+	Result auxLeft, auxRight;
+	if(numThreads<=1){// if we can only use 1 thread
+	    auxLeft = np_DC(vp,left,middle,numThreads);
+	    auxRight = np_DC(vp,middle+1, right, numThreads);//we have to add one because the middle point is includade in the left half
+	}
+	else{
+	    std:: thread t([&vp,&auxLeft,left,middle, numThreads]{
+	        vector <Point> v(vp);
+            auxLeft = np_DC(v,left,middle,numThreads/2.0);
+	    });
+        auxRight = np_DC(vp,middle+1, right, numThreads/2.0);//we have to add one because the middle point is includade in the left half
+	    t.join();
+	}
+
+	// Select the best solution from left and right, the smaller is the better one
+	if(auxLeft.dmin<auxRight.dmin){
+	    res = auxLeft;
+	}
+	else
+	    res = auxRight;
 
 	// Determine the strip area around middle point
-	// TODO
+	vector<Point> strip;//vector where we will add all the points that can belong to the strip
+	for(int i=left; i<= right; i++){
+	    if(abs(vp[middle].x-vp[i].x)<=res.dmin){//if this happens it means it can belong to the strip area
+            strip.push_back(vp[i]);
+	    }
+	}
 
 	// Order points in strip area by Y coordinate
-	// TODO
+	sortByY(strip,0,strip.size()-1);
 
 	// Calculate nearest points in strip area (using npByY function)
-	// TODO
+	npByY(strip,0,strip.size()-1,res);
 
 	// Reorder points in strip area back by X coordinate
-	//TODO
+	sortByX(strip,0,strip.size()-1);
 
-	//return res;
+	return res;
 }
 
 
